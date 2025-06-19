4f810d8ea84b524eee70e8a53258c29db8d41b60
Jang0seon <90378719+Jang0seon@users.noreply.github.com>
2025-06-19 18:54:28 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,3 +25,24 @@ console.log("GitHub Pages에서 실행되는 JS 파일입니다.");
   targetElements.forEach((element) => {
     observer.observe(element);
   });
+
+/* 모바일 진동 */
+navigator.vibrate = navigator.vibrate | | navigator.webkitVibrate | | navigator.mozVibrate | | navigator.msVibrate; // 작동되는 진동 메소드가 다르므로 통합
+function vibrate() {
+  if (navigator.vibrate) {
+    navigator.vibrate(20000); // 진동을 울리게 한다. 1000ms = 1s 이다.
+  } else {
+    alert("진동을 지원하지 않는 기종 입니다.");
+  }
+}
+ 
+function vibratearray() {
+  if (navigator.vibrate) {
+    navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]); // 배열로도 가능하다. 진동은 SOS 모스부호
+  } else {
+    alert("진동을 지원하지 않는 기종 입니다.");
+  }
+}
+function stop() {
+  navigator.vibrate(0); // (0) 대신에 ([]) 을 넣어도 동일하다.
+}
