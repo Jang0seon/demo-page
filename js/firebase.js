248f5f4fa24433f863924c33ed8581322b9323ff
mong-it <aodlvnf2588@gmail.com>
2025-06-28 13:06:03 +0900

    test firebase

---
+++ js/firebase.js
@@ -0,0 +1,35 @@
+import firebase from 'firebase/compat/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+apiKey: "AIzaSyDfYJRdcK97ZuqglKaVTR1eN8b-8Jdv1q4",
+    authDomain: "tokyo-micron-463601-p8.firebaseapp.com",
+    projectId: "tokyo-micron-463601-p8",
+    storageBucket: "tokyo-micron-463601-p8.firebasestorage.app",
+    messagingSenderId: "240808159243",
+    appId: "1:240808159243:web:df3412577264a620a6ff0e",
+    measurementId: "G-YLG46GVZN8"
+};
+
+export default firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = firebase.firestore()
+const likeRef = db.collection("likes").doc("main");
+
+function updateLikeCount() {
+likeRef.get().then(doc => {
+    if (doc.exists) {
+    document.getElementById("like-count").textContent = doc.data().count;
+    } else {
+    likeRef.set({ count: 0 });
+    }
+});
+}
+
+function like() {
+likeRef.update({ count: firebase.firestore.FieldValue.increment(1) });
+updateLikeCount();
+}
+
+updateLikeCount(); // 초기 좋아요 수 가져오기
